69759a9834e1806f8dfaa99c8b0091c8c716b46f
Biplob798 <mdbiplop798@gmail.com>
2023-11-17 23:53:15 +0600

    verify jwt toke and create middleware admin veryify

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -26,13 +27,122 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+
         await client.connect();
 
 
         // db collection 
         const menuCollection = client.db("bistroDB")
             .collection("menu");
+        const userCollection = client.db("bistroDB")
+            .collection("users");
+        const reviewsCollection = client.db("bistroDB")
+            .collection("reviews");
+        const cartsCollection = client.db("bistroDB")
+            .collection("carts");
+
+
+
+
+
+
+        // jot related api 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '24d'
+            })
+            res.send({ token })
+        })
+
+        //  middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization)
+            if (!req.headers.authorization)
+                return res.status(401).send({ message: 'unauthorized access' })
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded
+                next()
+            });
+
+        }
+
+        // use verify admin after verify token 
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
+
+        // user related api 
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        // check admin 
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            // insert email if user doesnt exists:
+            // i can do this many ways (1.email unique,2.upsert,3.simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        // Admin Api  
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // menu related api 
 
         // get menu data 
         app.get('/menu', async (req, res) => {
@@ -41,8 +151,29 @@ async function run() {
         })
 
         // get views data 
-        app.get('/menu', async (req, res) => {
-            const result = await menuCollection.find().toArray()
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await cartsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // carts collection 
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body
+            const result = await cartsCollection.insertOne(cartItem);
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cartsCollection.deleteOne(query)
             res.send(result)
         })
 
@@ -51,14 +182,10 @@ async function run() {
 
 
 
-
-
-
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
+
         // await client.close();
     }
 }
@@ -73,3 +200,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Bistro boss is setting on port ${port}`)
 })
+
+
+
+/***
+ * app.get('/user')
+ * 
+ * app.get('/user/:id')
+ * 
+ * app.post('/user')
+ * app.put('/user/:id')
+ * 
+ * app.patch('/user/:id')
+ * app.delete('/user/:id')
+ * 
+ * 
+ * 
+ * 
+*/
