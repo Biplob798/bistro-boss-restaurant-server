ad9fdd3c9915be25038ad5634d455274f93c2804
Biplob798 <mdbiplop798@gmail.com>
2023-11-21 18:47:47 +0600

    post payment in db and get data from db for payment by use stripe.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 
@@ -224,7 +225,21 @@ async function run() {
 
 
 
+        // payment api intent 
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body
+            const amount = parseInt(price * 100)
+            console.log(amount, 'amount inside the intent')
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card'],
 
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
 
 
         await client.db("admin").command({ ping: 1 });
